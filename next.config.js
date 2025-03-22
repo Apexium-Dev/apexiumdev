01b8426aa25d7a421988addb457718cd49333989
mahmutmft <miftarosky@hotmail.com>
2025-03-22 06:20:53 +0100

    fix: update config for static export and fix ESLint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     remotePatterns: [],
-    unoptimized: false,
+    unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
@@ -13,40 +14,6 @@ const nextConfig = {
     scrollRestoration: true,
   },
   compress: true,
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          }
-        ],
-      },
-      {
-        source: '/fonts/:all*',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          }
-        ],
-      },
-      {
-        source: '/_next/image/:all*',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          }
-        ],
-      },
-    ];
-  },
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       config.optimization.splitChunks.cacheGroups = {
